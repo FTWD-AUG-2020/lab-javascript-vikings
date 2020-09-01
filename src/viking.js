497fade3c63de3b5ddf8081f9385d6e3af1a4e9a
matheus <mtava002@gmail.com>
2020-09-01 16:26:32 -0400

    iteration 1

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,24 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health
+        this.strength = strength
+    }
+    attack = () => {
+        return this.strength
+    }
+    receiveDamage(dmg) {
+        this.health -= dmg
+    }
 
+
+}
+let Arnie = new Soldier(800, 80);
 // Viking
-class Viking {}
+class Viking { }
 
 // Saxon
-class Saxon {}
+class Saxon { }
 
 // War
-class War {}
+class War { }
